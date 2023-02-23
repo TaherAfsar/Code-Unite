235520ee6d9db9cd937474abe265f7f1b2cb2181
Shubham Gaikwad <99805248+sghere@users.noreply.github.com>
2023-02-24 00:27:35 +0530

    Minor important Changes

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -1,32 +1,28 @@
-import Message from "../models/Message.js";
+import Message from "../models/message.js";
 
-export const createNewMessage = async (req,res) => {
-    const body = req.body;
+export const createNewMessage = async (req, res) => {
+  const body = req.body;
 
   try {
     const result = await Message.create({
-        roomId: body.roomId,
-        senderId: body.senderId,
-        senderName: body.senderName,
-        text: body.text
-    })
+      roomId: body.roomId,
+      senderId: body.senderId,
+      senderName: body.senderName,
+      text: body.text,
+    });
     return res.status(200).json(result);
   } catch (e) {
     res.status(500).json(e);
   }
 };
 
-
-export const getMessages = async (req,res) => {
-    try{
-    const messages = await Message.find({ roomId: req.params.roomId});
+export const getMessages = async (req, res) => {
+  try {
+    const messages = await Message.find({ roomId: req.params.roomId });
     res.status(200).json(messages);
-    }
-    catch(e)
-    {
-        res.status(500).json(e);
-    }
+  } catch (e) {
+    res.status(500).json(e);
+  }
 };
 
-
-export default {createNewMessage,getMessages};
+export default { createNewMessage, getMessages };
