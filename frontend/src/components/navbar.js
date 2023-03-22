0f37c93392ad2941d4bd828a079bda96e1694641
Taher <taherafsar41@gmail.com>
2023-03-22 22:17:03 +0530

    minor changes

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -10,17 +10,30 @@ function NavBar() {
         <Box>
           {/* <Image src={'../src/assets/logo.jpeg'} alt="My Logo" h="8" /> */}
           {/* <img src="../src/assets/logo.jpeg" alt="react logo" style={{ width: '400px', }}/> */}
-          <Link to= "/Home">
-            <Image width={200} height={'150px'} alt='logo' src={logo} mt='30px' ml= '10px'/>
-            </Link>
+          <Link to="/Home">
+            <Image
+              width={200}
+              height={"150px"}
+              alt="logo"
+              src={logo}
+              mt="30px"
+              ml="10px"
+            />
+          </Link>
         </Box>
         <Spacer />
         <Stack direction="row" spacing="7">
           <Link to="/joinroom">Join Room</Link>
           <Link to="/createroom">Create Room</Link>
-          <Link to="../pages/ProblemStatement">Problem Statements</Link>
+          {/* <Link to="../pages/ProblemStatement">Problem Statements</Link> */}
           <Link to="../pages/">
-          <Image width={'40px'} height={'50px'} alt='logo' src={profileIcon1} mt = '-25px'/>
+            <Image
+              width={"40px"}
+              height={"50px"}
+              alt="logo"
+              src={profileIcon1}
+              mt="-25px"
+            />
           </Link>
           {/* <Link to="/contact">Contact</Link> */}
         </Stack>
