73319ed10f478a95ffefec3e1bf94b9274fe884c
Taher <taherafsar41@gmail.com>
2023-03-24 21:27:26 +0530

    added editor and compiler

--- frontend/src/components/editor.js
+++ frontend/src/components/editor.js
@@ -1,76 +1,201 @@
-import React, { useState } from "react";
-import AceEditor from "react-ace";
+import { useState } from "react";
 import {
   Box,
+  Text,
+  Flex,
   Button,
-  FormControl,
-  FormLabel,
+  IconButton,
+  Input,
   Select,
-  Textarea,
+  Stack,
+  useToast,
 } from "@chakra-ui/react";
+import { MdRefresh } from "react-icons/md";
+import AceEditor from "react-ace";
 
-import "ace-builds/src-noconflict/mode-c_cpp";
-import "ace-builds/src-noconflict/mode-java";
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/mode-csharp";
-
+import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-monokai";
 
-function CodeEditor() {
-  const [code, setCode] = useState("");
-  const [mode, setMode] = useState("javascript");
-  const [output, setOutput] = useState("");
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const API_URL = "https://api.jdoodle.com/v1/execute";
 
-  const handleModeChange = (newMode) => {
-    setMode(newMode);
+const defaultCode = 'print("hello world");';
+
+const Editor = () => {
+  const [code, setCode] = useState(defaultCode);
+  const [language, setLanguage] = useState("python3");
+  const [consoleLogs, setConsoleLogs] = useState([]);
+  const toast = useToast();
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
   };
 
-  const executeCode = () => {
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+  };
+
+  const handleReset = () => {
+    setCode(defaultCode);
+    setConsoleLogs([]);
+  };
+  const handleExecute = async () => {
+    setConsoleLogs([]);
+
     try {
-      const result = eval(code);
-      setOutput(result.toString());
+      let program = {
+        script: code,
+        language: language,
+        versionIndex: "0",
+        clientId: "5715b31dddb014988ed4e6b8f1409111",
+        clientSecret:
+          "d5568ed6f786da78557ab96e073907847f007e721bb0f53effd40b4870caaa84",
+      };
+
+      if (language === "python3") {
+        program = {
+          ...program,
+          stdin: "",
+        };
+      } else if (language === "java") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "3",
+          script: { code },
+        };
+      }
+
+      //  else if (language === "java") {
+      //   program = {
+      //     ...program,
+      //     stdin: "",
+      //     versionIndex: "3",
+      //     script: `public class Main { public static void main(String[] args) { ${code} } }`,
+      //   };
+      // }
+      else if (language === "nodejs") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "2",
+        };
+      } else if (language === "ruby") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "3",
+        };
+      } else if (language === "c") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "0",
+        };
+      } else if (language === "cpp") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "4",
+        };
+      } else if (language === "csharp") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "1",
+        };
+      } else if (language === "swift") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "3",
+        };
+      } else if (language === "php") {
+        program = {
+          ...program,
+          stdin: "",
+          versionIndex: "3",
+        };
+      }
+
+      const response = await fetch(`${PROXY_URL}${API_URL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(program),
+      });
+
+      const data = await response.json();
+
+      setConsoleLogs([data.output]);
     } catch (error) {
-      setOutput(error.message);
+      console.error(error);
+      toast({
+        title: "Error",
+        description: "Failed to execute the code.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
     }
   };
 
   return (
-    <Box
-      mt={8}
-      maxW="800px"
-      mx="auto"
-      bg="white"
-      p={6}
-      borderRadius="md"
-      boxShadow="md"
-    >
-      <FormControl>
-        <FormLabel htmlFor="language">Language</FormLabel>
-        <Select
-          id="language"
-          value={mode}
-          onChange={(e) => handleModeChange(e.target.value)}
-        >
-          <option value="c_cpp">C++</option>
-          <option value="java">Java</option>
-          <option value="javascript">JavaScript</option>
-          <option value="csharp">C#</option>
-        </Select>
-      </FormControl>
-      <AceEditor
-        mode={mode}
-        theme="monokai"
-        onChange={(newCode) => setCode(newCode)}
-        value={code}
-        height="600px"
-        width="100%"
-      />
-      <Button mt={4} onClick={executeCode}>
-        Run
-      </Button>
-      <Textarea mt={4} value={output} readOnly />
+    <Box w="100%" p="4" backgroundColor={"#9840db"}>
+      <Flex direction={["column", "row"]}>
+        <Stack w={["100%", "50%"]} p="2">
+          <Flex mb="2">
+            <Text mr="2">Language:</Text>
+            <Select value={language} onChange={handleLanguageChange}>
+              <option value="python3">Python 3</option>
+              <option value="java">Java</option>
+              <option value="nodejs">JavaScript</option>
+              <option value="ruby">Ruby</option>
+              <option value="c">C</option>
+              <option value="cpp">C++</option>
+              <option value="csharp">C#</option>
+              <option value="swift">Swift</option>
+              <option value="php">Php</option>
+            </Select>
+          </Flex>
+          <AceEditor
+            mode="python"
+            theme="monokai"
+            onChange={handleCodeChange}
+            value={code}
+            width="100%"
+            height="400px"
+            name="editor"
+            editorProps={{ $blockScrolling: true }}
+          />
+          <Flex mt="4">
+            <Button onClick={handleExecute}>Run</Button>
+            <IconButton
+              ml="2"
+              aria-label="Reset Code"
+              icon={<MdRefresh />}
+              onClick={handleReset}
+            />
+          </Flex>
+        </Stack>
+        <Stack w={["100%", "50%"]} p="2">
+          <Text mb="2">Console Logs:</Text>
+          <Box
+            borderWidth="1px"
+            borderRadius="md"
+            p="2"
+            h="400px"
+            overflowY="scroll"
+          >
+            {consoleLogs.map((log, index) => (
+              <Text key={index}>{log}</Text>
+            ))}
+          </Box>
+        </Stack>
+      </Flex>
     </Box>
   );
-}
+};
 
-export default CodeEditor;
+export default Editor;
